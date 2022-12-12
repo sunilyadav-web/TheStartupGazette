dd149412447e577735699b281972cb79f2bd2dc1
Sunil Yadav <sunilyadavwork2003@gmail.com>
2022-12-12 16:19:32 +0530

    created model and adding static media file settings

--- dailynews/settings.py
+++ dailynews/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from django.contrib import messages
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
@@ -110,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -121,8 +123,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'Data')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR / "static")
+ ]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
--- home/models.py
+++ home/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+from .utils import *
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name=models.CharField(max_length=100, verbose_name="Category Name ")
+    description=models.CharField(max_length=250, verbose_name="Category Description")
+    created_at=models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at=models.DateTimeField(auto_now=True, editable=False)
+
+    def __str__(self):
+        return self.name
+
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+) 
+
+class Post(models.Model):
+    title=models.CharField(max_length=600, unique=True)
+    image=models.ImageField(upload_to='post_images',null=True,blank=True)
+    content=RichTextField()
+    category=models.ForeignKey(Category,on_delete=models.SET_NULL, null=True,blank=True)
+    meta_description=models.TextField(blank=True,null=True)
+    meta_keyword=models.TextField(blank=True,null=True)
+    slug=models.SlugField(max_length=1000, unique=True,null=True,blank=True)
+    status=models.IntegerField(choices=STATUS, default=0)
+    publish_date=models.DateTimeField()
+    writer=models.ForeignKey(User,on_delete=models.SET_NULL,null=True,blank=True)
+    created_at=models.DateTimeField(auto_now_add=True,editable=False)
+    updated_at=models.DateTimeField(auto_now=True, editable=False)
+
+    @property
+    def imageURL(self):
+        try:
+            URL=self.image.url
+        except:
+            URL=''
+        return URL
+
+    class Meta:
+        ordering = ['-created_at']  
+
+    def __str__(self):
+        return self.title 
+
+    def save(self,  *args, **kwargs):
+        s=generate_slug(self.title)
+        print('this is slug',s)
+        self.slug=s
+        super(Post,self).save(*args, **kwargs)
+
+
+
