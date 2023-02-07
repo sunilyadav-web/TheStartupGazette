2875d24252d7d609828ff92db0dd28f88db4077f
Sunil Yadav <90596707+sunilyadav-web@users.noreply.github.com>
2023-02-08 01:06:06 +0530

    added production database

--- dailynews/settings.py
+++ dailynews/settings.py
@@ -85,10 +85,23 @@ WSGI_APPLICATION = 'dailynews.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+#Production Database 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tsg',
+        'USER': 'tsgharshsunil',
+        'PASSWORD': 'tsgharshdeveloper123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
