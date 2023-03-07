12b22759cf8bb710afce311ab4f5bbe00d38ff30
Sunil Yadav <90596707+sunilyadav-web@users.noreply.github.com>
2023-03-08 00:05:24 +0530

    Added footer menu in Django menu.

--- home/menus.py
+++ home/menus.py
@@ -21,5 +21,23 @@ MENUS = {
             "url": "home:contact"
         },
     ],
+    'FOOTER_LEFT_MENU' : [
+        {
+            "name": "About Us",
+            "url": "home:about_us",
+        },
+        {
+            "name": "Privacy Policy",
+            "url": "home:privacy_policy",
+        },
+        {
+            "name": "Terms and Condition",
+            "url": "home:term_and_condition",
+        },
+        {
+            "name": "Contact",
+            "url": "home:contact",
+        }
+    ]
 
 }
