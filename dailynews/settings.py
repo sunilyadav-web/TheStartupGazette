6d997a45e8daffa49bb4450724d19a48f938a405
Sunil Yadav <90596707+sunilyadav-web@users.noreply.github.com>
2023-03-08 22:55:58 +0530

    Updated custom processor (2 of 2)

--- dailynews/settings.py
+++ dailynews/settings.py
@@ -65,7 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'home.processor.globalContext',  # custom context_processor
+                'home.processor.custom_processor',  # custom context_processor
             ],
         },
     },
