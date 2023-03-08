40b88066dfd729d40f3ea7cab6adfe6625a3a4fb
Sunil Yadav <90596707+sunilyadav-web@users.noreply.github.com>
2023-03-08 22:35:52 +0530

    improved sitemap code.

--- home/sitemap.py
+++ home/sitemap.py
@@ -2,31 +2,35 @@ from django.contrib.sitemaps import Sitemap
 from django.shortcuts import reverse
 from .models import *
 
+
 class StaticViewSitemap(Sitemap):
     def items(self):
-        urls= ['home:home','home:about_us','home:search','home:term_and_condition','home:privacy_policy']
-        return urls   
-        
+        urls = ['home:home', 'home:about_us', 'home:search', 'home:term_and_condition', 'home:privacy_policy']
+        return urls
+
     def location(self, item):
         return reverse(item)
+
 
 class PostSiteMap(Sitemap):
     def items(self):
         return Post.objects.all()
-    
-    def location(self,item):
-        return '/post/%s' % (item.slug)
+
+    def location(self, item):
+        return '/post/%s' % item.slug
+
 
 class CategorySiteMap(Sitemap):
     def items(self):
         return Category.objects.all()
 
-    def location(self,item):
-        return '/category/%s' %(item.name)
-    
+    def location(self, item):
+        return '/category/%s' % item.name
+
+
 class TagSiteMap(Sitemap):
     def items(self):
-        return  Tag.objects.all()
-    
-    def location(slef,item):
-        return '/tag/%s'%(item.name)
+        return Tag.objects.all()
+
+    def location(self, item):
+        return '/tag/%s' % item.name
